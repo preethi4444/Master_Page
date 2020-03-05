322a6fe5586fb32dcb08c58ee172c360516fa097
SaiPreethi Induri <Saipreethi_Induri@epam.com>
2020-03-05 16:34:38 +0530

    Add project files.

---
+++ Master_Page/App_Start/Validation.js
@@ -0,0 +1,13 @@
+﻿function funValid() {
+    var name = $.trim($("#username").val());
+    if (!name) {
+        $("#Label1").text("*Please enter your first name")
+        return false;
+    }
+    var lname = $.trim($("#password").val());
+    if (!lname) {
+        $("#Label2").text("*Please enter your last name")
+        return false;
+    }
+    return true;
+}
